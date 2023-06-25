984a60042e81170655d83a5100eca23ad66aea34
Mark Soro <napoleon_soro@rocketmail.com>
2023-06-25 10:05:28 -0400

    added 03_14 challenge

---
+++ 03_14/Book.js
@@ -0,0 +1,29 @@
+// CREATE AT LEAST 5 BOOK OBJECT
+// ADD PROPERTIES AND METHODS
+
+class Book {
+  constructor(
+    // Add Parameters
+    title,
+    author,
+    price,
+    onSale
+  ) {
+    // Add Properties
+    (this.title = title),
+      (this.author = author),
+      (this.price = price),
+      (this.onSale = onSale);
+  }
+  // Add Methods
+  priceOnSale(discount) {
+    if (this.onSale)
+      return (
+        "Discounted price is " +
+        (this.price - this.price * (discount / 100)).toString()
+      );
+    else return "Book NOT on sale";
+  }
+}
+
+export default Book;
---
+++ 03_14/script.js
@@ -0,0 +1,50 @@
+/**
+ * Challenge: Create a new object type
+ *
+ * - Create a new object type "Book" using a class or an object constructor function.
+ * - Add at least 5 book objects.
+ */
+
+import Backpack from "./Backpack.js";
+import Book from "./Book.js";
+
+const everydayPack = new Backpack(
+  "Everyday Backpack",
+  30,
+  "grey",
+  15,
+  26,
+  26,
+  false,
+  "December 5, 2018 15:00:00 PST"
+);
+
+console.log("The everydayPack object:", everydayPack);
+console.log("The pocketNum value:", everydayPack.pocketNum);
+console.log("Days since aquired:", everydayPack.backpackAge());
+
+const macbeth = new Book("Macbeth", "William Shakespeare", 30, true);
+const grapesOfWrath = new Book("Grapes of Wrath", "John Steinbeck", 25, false);
+const toKillAMockingbird = new Book(
+  "To Kill A Mocking Bird",
+  "Harper Lee",
+  20,
+  true
+);
+const ofMiceAndMen = new Book("Of Mice and Men", "John Steinbeck", 10, true);
+const braveNewWorld = new Book("Brave New World", "Aldous Huxley", 15, false);
+
+const bookCase = [
+  macbeth,
+  grapesOfWrath,
+  toKillAMockingbird,
+  ofMiceAndMen,
+  braveNewWorld,
+];
+console.log(bookCase);
+
+for (let b in bookCase) {
+  console.log("The title is :", bookCase[b].title);
+  console.log(bookCase[b].title, "is on sale? : ", bookCase[b].onSale);
+  console.log(bookCase[b].priceOnSale(20));
+}
